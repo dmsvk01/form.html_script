3197785268bfd789e52294d5f8cbd2667ad34df1
dmsvk01 <62129820+dmsvk01@users.noreply.github.com>
2020-08-13 11:51:18 +0900

    Create form_ok.php

---
+++ WebContent/form_ok.php
@@ -0,0 +1,28 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>   님의 개인정보 입력사항</h1>
+
+<p>company : </p>
+<p>First name : </p>
+<p>Last name : </p>
+<p>email : </p>
+<p>title : </p>
+<p>phone : </p>
+<p>cr : </p>
+<p>date : </p>
+<p>job : </p>
+<p>dr : </p>
+<p>expectations : </p>
+
+
+
+</body>
+</html>
